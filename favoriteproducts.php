695dec9837977f8be14ce6c77a30afa34ea5d9fe
Alex Sampaio <alex.sampaio1991@gmail.com>
2017-02-01 17:39:01 +0100

    // Version updated to v1.3.1

--- favoriteproducts.php
+++ favoriteproducts.php
@@ -33,7 +33,7 @@ class FavoriteProducts extends Module
 	{
 		$this->name = 'favoriteproducts';
 		$this->tab = 'front_office_features';
-		$this->version = '1.3.0';
+		$this->version = '1.3.1';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
